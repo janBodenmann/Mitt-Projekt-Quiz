bd9ab9e81999176c7491cac9b274e531e2dfa80b
brettkanker <stanisic.luka44@gmail.com>
2023-03-22 10:33:50 +0100

    highscore database

--- end.js
+++ end.js
@@ -23,5 +23,29 @@ gameTwo.textContent = "Zweites Spiel: " + points2
 
 total.textContent = "Total: " + totalPoints
 
+const mysql = require('mysql');
+
+// create a connection to the MySQL database
+const connection = mysql.createConnection({
+  host: '10.5.43.26',
+  user: 'root',
+  password: '',
+  database: 'quiz'
+});
+
+// update the score for the user with ID 1
+const score = 100;
+const userId = 1;
+
+const query = `UPDATE accounts SET score = ${score} WHERE username = t`;
+connection.query(query, (error, results) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('Score updated successfully!');
+  }
+});
+
+
 // Local Storage wird gelöscht
 localStorage.clear()
